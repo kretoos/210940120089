195914232a9536d41fab578f86075bc445b080f9
Kartikey Jain <97635532+kretoos@users.noreply.github.com>
2022-01-30 14:53:20 +0530

    Add files via upload

---
+++ 210940120089/sum.java
@@ -0,0 +1,15 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class sum {
+    public static void main(String[] args){
+        // create scanner's instance to attach scanner to stdin
+        Scanner ss = new Scanner(System.in);
+        System.out.println("Enter the of id the employee");
+        int id = ss.nextInt();
+        System.out.println("Enter the name of the employee");
+        String name = ss.next();
+        double salary;
+    }
+}
